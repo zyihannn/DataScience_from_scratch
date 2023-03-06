e7b2ebb3c848a406465434e98119b32b9106e848
zyihannn <crystalzyihan@gmail.com>
2023-03-05 16:18:53 -0800

    add multiple linear regression  notes

---
+++ datascience_from_scratch/multiple_linear_regression.py
@@ -0,0 +1,68 @@
+import numpy as np
+from typing import List 
+import random
+
+# multiple linear regression:
+#y_i
+#coefficient -> beta = [alpha, beta_1,beta_2 .... beta_k]
+# x_i = [1, x_i1,  x_i2, ..... x_ik]
+# y_i = beta * x_i (dot product)
+
+# [ x,x,x,x,..x      [ alpha,        [ y 
+#   x,x,x,x,..x,  *    beta_1,    =    y 
+#   x,x,x,x,..x          ...          ..
+#   ....]              beta_k ]        y ]
+
+Vector = List[float]
+
+#### fitting : using gradient descend to find out least sqare errors 
+def squared_error(x: Vector, y : float, beta: Vector) -> float:
+    return (np.dot(beta,x)-y)**2 #this is just for one x, one y 
+
+#for entire data set:
+def sum_squared_error(X:List[Vector], Y:Vector, beta:Vector) -> float:
+    return np.sum([squared_error(x, y , beta) for x, y in zip(X,Y)])
+
+#calculate gradient of sum of square error 
+def grad_sum_squared_error(X:List[Vector], Y:Vector, beta:Vector) -> float: 
+    #error =  np.dot(beta,x) - y 
+    #sqr_error  = (error)**2
+    #grad_wrt_beta = (2*error)* np.sum(beta)
+
+
+    # x is vector, y is float 
+    return np.sum([2*(np.dot(beta,x) -y)*np.sum(beta) for x, y in zip(X,Y)])
+
+####gradient descend 
+# make a fake dataset with perfect fit
+k = 5 
+m = 10 
+X=[]
+for _ in range(0,10):
+    X.append([random.uniform(-50,50) for _ in range(0, k)])
+
+beta_actual = [1,2,3,4,5]
+Y= np.dot(X,beta_actual)
+
+
+#start point  :
+beta = [random.random() for _ in range(k)]
+learning_rate = -0.001
+attemps = 500
+
+for _ in range(0,attemps):
+    grad = grad_sum_squared_error(X, Y, beta)
+
+    beta = np.array(beta) + np.array([grad]*len(beta))*learning_rate
+    print(beta)
+
+
+
+
+
+
+
+
+
+
+
